40cc1539d74a0f0d62257c6280b8da2c929e8a51
THIBAULT COUTAZ <coutaz.thibault@laposte.net>
2015-09-27 19:40:41 +0200

    Carte du monde vue centrée

--- TP1MG2/Renderer/RendererConfig3.h
+++ TP1MG2/Renderer/RendererConfig3.h
@@ -6,7 +6,7 @@ class RendererConfig3 : public Renderer
 {
 public:
 	RendererConfig3() :Renderer(1280, 720,
-		Camera(Vector3(1080., 1080., 600.), Vector3(1080, 540., 0.), 1., Vector3(0., 0., -1.)),
+		Camera(Vector3(1500., 1080., 600.), Vector3(1500, 540., 0.), 1., Vector3(0., 0., -1.)),
 		Vector3(500., 500., 1000.), // Midi
 		//Vector3(-100., 1100., 500.), // Couché de soleil
 		new TerrainHeightmap(QImage("Resources/world_small.png"), false, 0.1)){};
