d118a3afdd4f7eba715a2f567341f83a31cfaf7e
Joris HORMANCEY <hmasterjo@hotmail.com>
2015-10-02 19:33:53 +0200

    Amélioration mineure RendererConfig5

--- TP1MG2/Renderer/RendererConfig5.h
+++ TP1MG2/Renderer/RendererConfig5.h
@@ -6,8 +6,8 @@ class RendererConfig5 : public Renderer
 {
 public:
 	RendererConfig5() : Renderer(1280, 720,
-		Camera(Vector3(-20., 1000., 700.), Vector3(1000., 1000., 0.), 1., Vector3(0., 0., -1.)),
-		Vector3(500., 1000., 1000.), // Midi
+		Camera(Vector3(-200., 1000., 900.), Vector3(1200., 1000., 0.), 1., Vector3(0., 0., -1.)),
+		Vector3(1000., 1000., 1100.), // Midi
 		//Vector3(-100., 1100., 500.), // Couche de soleil
 		new TerrainFractal(2000, 2000)){};
 
--- TP1MG2/main.cpp
+++ TP1MG2/main.cpp
@@ -23,36 +23,36 @@ int main(int argc, char *argv[])
 {
 	srand(time(0));
 	QApplication a(argc, argv);
-	Debug d;
-	Terrain * t2 = new TerrainFractal(1000, 1000); // Terain créé grace à une fonction
-	QTextEdit * logTxt = new QTextEdit();
 	QLabel l;
-	HeightmapWidget * hmw = new HeightmapWidget(t2, 0);
+	QTextEdit * logTxt = new QTextEdit();
 
-	// Choix de la configuration à afficher (de RendererConfig1 à RendererConfig5)
-	// Changer les couleurs dans Terrain.getColor()
-	Renderer * r = new RendererConfig1();
-
-	// Mesures
-	Mesure m;
-	//m.execMesure();
+	// OPENGL
+	//Terrain * t2 = new TerrainFractal(1000, 1000); // Terain créé grace à une fonction
+	//HeightmapWidget * hmw = new HeightmapWidget(t2, 0);
+	//hmw->show();
+	//delete t2;
 
 	// DEBUG
-	d.execTest(logTxt);
-	logTxt->show();
+	//Debug d;
+	/*d.execTest(logTxt);
+	logTxt->show();*/
+
+	// Mesures
+	//Mesure m;
+	//m.execMesure();
+	
+	// Choix de la configuration à afficher (de RendererConfig1 à RendererConfig5)
+	// Changer les couleurs dans Terrain.getColor()
+	Renderer * r = new RendererConfig5();
 
 	// Raytracage du terrain
 	r->Raytrace();
 	l.setPixmap(QPixmap::fromImage(r->getScreen()));
 	l.show();
-
-	
-    hmw->show();
-	delete t2;
 	
 	a.exec();
 	delete logTxt;
-	delete hmw;
+	//delete hmw;
 	delete r;
 	return 0;
 	
